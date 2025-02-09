e5c7bb9a36318730aa920369f52a0d3f67f0b171
NikitaPirate <59086300+NikitaPirate@users.noreply.github.com>
2025-02-09 19:21:51 +0400

    Add server default 0 to account fields.

--- app/models.py
+++ app/models.py
@@ -24,10 +24,20 @@ class UUIDModel(SQLModel, table=False):
 class Account(UUIDModel, table=True):
     address: str = Field(index=True)
     chain: Chain = Field()
-    c_ratio: float = Field(default=0)
-    snx_count: condecimal(max_digits=50, decimal_places=0) = Field(default=0)
-    collateral: condecimal(max_digits=50, decimal_places=0) = Field(default=0)
-    sds_count: condecimal(max_digits=50, decimal_places=0) = Field(default=0)
-    debt: condecimal(max_digits=50, decimal_places=0) = Field(default=0)
-    claimable_snx: condecimal(max_digits=50, decimal_places=0) = Field(default=0)
+    c_ratio: float = Field(default=0, sa_column_kwargs={"server_default": "0"})
+    snx_count: condecimal(max_digits=50, decimal_places=0) = Field(
+        default=0, sa_column_kwargs={"server_default": "0"}
+    )
+    collateral: condecimal(max_digits=50, decimal_places=0) = Field(
+        default=0, sa_column_kwargs={"server_default": "0"}
+    )
+    sds_count: condecimal(max_digits=50, decimal_places=0) = Field(
+        default=0, sa_column_kwargs={"server_default": "0"}
+    )
+    debt: condecimal(max_digits=50, decimal_places=0) = Field(
+        default=0, sa_column_kwargs={"server_default": "0"}
+    )
+    claimable_snx: condecimal(max_digits=50, decimal_places=0) = Field(
+        default=0, sa_column_kwargs={"server_default": "0"}
+    )
     liquidation_deadline: datetime.datetime | None = Field(default=None)
